e0658f0bb8b35f5beb158fd4f626b66eb01376f5
George Kariuki <gconsummer.gk@gmail.com>
2024-07-27 03:19:18 +0300

    Add pickle models to app folder

---
+++ app/pickles.py
@@ -0,0 +1,9 @@
+import pickle
+
+# Load your pre-trained model from a pickle file
+with open('models/logreg_model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+# Load the vectorizer
+with open('models/vectorizer.pkl', 'rb') as file:
+    vectorizer = pickle.load(file)
