28306dcac3416b1faba03ab10d37393564595c9f
George Kariuki <32328769+ggeorgekkariuki@users.noreply.github.com>
2024-07-30 10:31:45 +0000

    Modify Contact page UI

--- app/page3.py
+++ app/page3.py
@@ -2,11 +2,21 @@ import streamlit as st
 
 st.header('Contacts:telephone:')
 
-st.markdown("""
-This project was created by:
-            
-1. Bradley Ouko :cat:[GitHub](https://github.com/Misfit911) :email: [Email](bradley.ouko@student.moringaschool.com, bradleyouko911@gmail.com)
-2. Ivy Atieng :cat:[GitHub](https://github.com/Atieng) :email: [Email](mailto:atiengivylisa@gmail.com)
-3. Clauda Sagini:cat: [GitHub](https://github.com/saginiclaudia):email:[Email](mailto:atiengivylisa@gmail.com)
-4. George Kariuki:cat: [GitHub](https://github.com/ggeorgekkariuki):email:[Email](mailto://ggeorgekkariuki@gmail.com)
-""")
+with st.container(border= True):
+    col1, col2, col3, col4 = st.columns(4)
+    with col2:
+        st.write("""
+                    Claudia Sagini :sunglasses: \n
+                    [Github Link](https://github.com/saginiclaudia) """)
+    with col3:
+        st.write("""
+                    Ivy Atieng :goggles:  \n
+                    [Github Link](https://github.com/Atieng)""")
+    with col4:
+        st.write("""
+                    George Kariuki :goggles: \n
+                    [GitHub Link](https://github.com/ggeorgekkariuki)""")
+    with col1:
+        st.write("""
+                    Bradley Ouko :sunglasses:  \n
+                    [GitHub Link](https://github.com/Misfit911)   """)
